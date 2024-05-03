1d47bbb454a34d37d9e1d887cd5f1d400e048bb7
Mithu Bortamuly <30743120+MithuBitz@users.noreply.github.com>
2024-05-03 23:37:03 +0530

    implement the musicPlayerService logics

--- musicApp/musicPlayerServices.js
+++ musicApp/musicPlayerServices.js
@@ -1,5 +1,38 @@
-import TrackPlayer from 'react-native-track-player';
+import TrackPlayer, {Event, RepeatMode} from 'react-native-track-player';
 
 import {playlistData} from './src/constants';
 
-export async function playbackService() {}
+//async function to setup the player
+export async function setupPlayer() {
+  const isSetup = false;
+  //If the player already ready then
+  try {
+    await TrackPlayer.getCurrentTrack();
+    isSetup = true;
+  } catch (error) {
+    await TrackPlayer.setupPlayer();
+    isSetup = true;
+  } finally {
+    return isSetup;
+  }
+}
+
+export async function addTracks() {
+  await TrackPlayer.add(playlistData);
+  await TrackPlayer.setRepeatMode(RepeatMode.Queue);
+}
+
+export async function playbackService() {
+  TrackPlayer.addEventListener(Event.RemotePause, () => {
+    TrackPlayer.pause();
+  });
+  TrackPlayer.addEventListener(Event.RemotePlay, () => {
+    TrackPlayer.play();
+  });
+  TrackPlayer.addEventListener(Event.RemoteNext, () => {
+    TrackPlayer.skipToNext();
+  });
+  TrackPlayer.addEventListener(Event.RemotePrevious, () => {
+    TrackPlayer.skipToPrevious();
+  });
+}
